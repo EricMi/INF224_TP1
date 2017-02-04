74da6301b42ad041ef02410dc027440430b5c171
EricMi <mipengfei1995@gmail.com>
2017-02-04 14:45:04 +0100

    Question 5: done!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define VERSION_BASIC
+#define VERSION_POLYMORPHISM
 
 #include <stdlib.h>
 #include <string>
@@ -8,12 +8,13 @@
 #include "Video.h"
 using namespace std;
 
-// Test for base version (Question 3)
+// Test for base class (Question 3)
 #ifdef VERSION_BASE
 int main() {
     Multimedia *m1 = new Multimedia();
     Multimedia *m2 = new Multimedia("eiffel", "/home/mi/workspace/INF224_TP1/files/eiffel.jpg");
 
+    cout << "Test for base class:" << endl;
     cout << "Multimedia object 1:" << endl;
     m1->print(cout);
     cout << "Multimedia object 1:" << endl;
@@ -49,4 +50,23 @@ int main() {
 
     return 0;
 }
-#endif
+#endif // VERSION_BASIC
+
+// Test for implementation of polymorphism (Question 5)
+#ifdef VERSION_POLYMORPHISM
+int main() {
+    int count = 0;
+    Multimedia ** myFiles = new Multimedia *[4];
+
+	myFiles[count++] = new Photo("eiffel", "~/workspace/INF224_TP1/files/eiffel.jpg", 1920, 1440);
+	myFiles[count++] = new Photo("telecom", "~/workspace/INF224_TP1/files/logo.png", 113, 113);
+	myFiles[count++] = new Video("rose", "~/workspace/INF224_TP1/files/TheRose-Westlife.mp4", 212);
+	myFiles[count++] = new Video("jj", "~/workspace/INF224_TP1/files/Twilight-JJLin.mp4", 312);
+
+    cout << "Test for implementation of polymorphism:" << endl;
+    for(int i = 0; i < count; i++) {
+        myFiles[i]->print(cout);
+        myFiles[i]->play();
+    }
+}
+#endif // VERSION_POLYMORPHISM
